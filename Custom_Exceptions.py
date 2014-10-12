6c1b32e131efe1d4f6e14cc41fefd81e75c9e22c
RyCSmith <rycsmith@gmail.com>
2014-10-12 13:42:14 -0400

    Completed File containing a number of classes of basic custom exceptions with descriptive titles for use in Library program.

---
+++ Custom_Exceptions.py
@@ -0,0 +1,9 @@
+class ClosedError( Exception ): pass
+
+class NoCurrentPatronError( Exception ): pass
+
+class NoBookNumber( Exception ): pass
+
+class TooShortError( Exception ): pass
+
+class MaxBooksError( Exception ): pass
